a8c995b3f32f845b1d77fba7c87c12e8341f749d
Sibsankar Chanda <sibsankar.besu@gmail.com>
2023-11-15 07:50:23 +0530

    Add money and swal implemented

--- src/app/add-money/add-money.component.ts
+++ src/app/add-money/add-money.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute  } from '@angular/router'; 
+import { AuthenticationService } from '../services/authentication.service';
+import { DashboardService } from '../services/dashboard.service';
+import { AddMoneyService } from '../services/add-money.service';
+import { Observable, of } from 'rxjs';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-add-money',
@@ -7,10 +13,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddMoneyComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute, private AuthGuardService: AuthenticationService, private AddMoneyService: AddMoneyService) { }
   public req_balance=false;
   public qr_code = true;
-  
+  public transfer_mode: string = "My value 1";
+  public addMoneyFormData = {
+    amount: '',
+    txn_number: '',
+    transfer_mode: '',
+    token: localStorage.getItem('token')
+  }
+  public error_msg = false;
+  public errorMsg = '';
   ngOnInit(): void {
   }
 
@@ -23,5 +37,43 @@ export class AddMoneyComponent implements OnInit {
     this.qr_code = true;
     this.req_balance = false;
   }
+
+
+  addMoneyToWallet(){
+    console.log(this.addMoneyFormData);
+    
+    this.AddMoneyService.addMoneyToWallet(this.addMoneyFormData).subscribe({
+      next: (v) => {
+        console.log(v.data.insertedData);
+        if(v.data.insertedData){
+          console.log(v.success);
+          Swal.fire({
+            title: "Success",
+            text: v.data.msg,
+            icon: "success"
+          });
+          this.router.navigate(['/add-money']);
+        }
+        
+      },
+      error: (e) => {
+        console.log(e.status);
+        console.log(e.status);
+          if(e.status==400){
+            this.error_msg = true;
+            this.errorMsg = "Please enter Registered Email id and Password";
+          }
+          if(e.status==401){
+            localStorage.clear();
+            this.error_msg = true;
+            this.errorMsg = "Please enter valid Registered Email id and Password";
+          }
+      },
+      complete: () => console.info('complete'),
+      
+  });
+  }
+
+  
 
 }
--- src/app/services/add-money.service.ts
+++ src/app/services/add-money.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddMoneyService {
+  httpOptions = {
+    headers: new HttpHeaders({
+      Authorization: this.CookieService.get('auth_key'),
+      'Access-Control-Allow-Origin': '*'
+    })
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient, private CookieService: CookieService) { }
+
+
+  private URL: string = environment.angApiUrl;
+
+  addMoneyToWallet(data: any): Observable<any> {
+    return this.http.post<any>(this.URL + 'addMoneyToWallet', data, this.httpOptions);
+
+  }
 }
